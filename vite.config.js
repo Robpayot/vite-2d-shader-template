2a111eb9f7ac09aa3ef9d4d77650062375a871b5
Robin payot <robpayot@gmail.com>
2023-02-02 17:03:34 +0100

    feat: set up 2D OGL template

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
-module.exports = {
+// vite.config.js
+import { defineConfig } from 'vite'
+import glsl from 'vite-plugin-glsl'
+import path from 'path'
+import babel from 'vite-plugin-babel'
+
+// https://vitejs.dev/config/
+export default defineConfig({
   root: 'src',
   base: './',
   build: {
     outDir: '../dist',
   },
-}
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  plugins: [babel(), glsl()],
+})
